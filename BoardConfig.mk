1cc16b3eb53865b370cb806b6c81e2372496fe55
ahmedkazim <ahmedkazim.ak@gmail.com>
2022-07-16 01:05:27 +0500

    Initial Bringup

---
+++ BoardConfig.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm6250-common
+include device/xiaomi/sm6250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/miatoll
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# HIDL
+ODM_MANIFEST_SKUS += \
+    nfc
+
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_miatoll
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_miatoll
+
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse,miatoll
+
+# Inherit proprietary blobs
+include vendor/xiaomi/miatoll/BoardConfigVendor.mk
